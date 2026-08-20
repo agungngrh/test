d02cb1f5f155afd71da73fea15cb2b597c5ad7db
AgungNugrahaa <anugrah171819@gmail.com>
2026-08-20 14:55:07 +0700

    Tambah dukungan delimiter kustom

--- etl.py
+++ etl.py
@@ -4,8 +4,8 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def extract_data(source: str) -> pd.DataFrame:
-    return pd.read_csv(source)
+def extract_data(source):
+    return pd.read_csv(source, delimiter=";")
 
 
 def transform(df: pd.DataFrame) -> pd.DataFrame:
