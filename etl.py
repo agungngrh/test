e8605eaf05a814d65e86c36126ce2014e04e622d
AgungNugrahaa <anugrah171819@gmail.com>
2026-08-20 14:31:47 +0700

    Tambah fungsi load_data untuk simpan hasil ke CSV

--- etl.py
+++ etl.py
@@ -7,3 +7,7 @@ def extract_data(source: str) -> pd.DataFrame:
 
 def transform(df: pd.DataFrame) -> pd.DataFrame:
     return df.dropna()
+
+
+def load(df: pd.DataFrame, destination: str):
+    return df.to_csv(destination, index=False)
